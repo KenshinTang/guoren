56cd7c912a10e69736143cf3e1ee73bee137e09d
KenshinTang <28135055@qq.com>
2019-10-31 20:37:42 +0800

    convert webp to png before upload to oss.

--- app/src/main/java/com/yunlinker/upimage/aliupload/OssService.java
+++ app/src/main/java/com/yunlinker/upimage/aliupload/OssService.java
@@ -1,5 +1,8 @@
 package com.yunlinker.upimage.aliupload;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.text.TextUtils;
 import android.util.Log;
 
 import com.alibaba.sdk.android.oss.ClientException;
@@ -13,6 +16,7 @@ import com.alibaba.sdk.android.oss.model.PutObjectResult;
 import com.yunlinker.upimage.UpImger;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.Map;
 
@@ -60,9 +64,16 @@ public class OssService {
             return;
         }
 
+        String uploadFileStr = localFile;
+        if (localFile.endsWith(".webp")) {
+            uploadFileStr = convertWebpToPng(localFile);
+        }
+        if (TextUtils.isEmpty(uploadFileStr)) {
+            uploadFileStr = localFile;
+        }
 
         // 构造上传请求
-        PutObjectRequest put = new PutObjectRequest(bucket, object, localFile);
+        PutObjectRequest put = new PutObjectRequest(bucket, object, uploadFileStr);
 
 
         // 异步上传时可以设置进度回调
@@ -86,4 +97,22 @@ public class OssService {
         });
     }
 
+    private String convertWebpToPng(String webpFilePath) {
+        Log.d("kenshin", "webpFilePath = " + webpFilePath);
+        String pathWithoutSuffix = webpFilePath.substring(0, webpFilePath.lastIndexOf("."));
+        String outputPngPath = pathWithoutSuffix + ".png";
+
+        try {
+            FileOutputStream out = new FileOutputStream(outputPngPath);
+            Bitmap bitmap = BitmapFactory.decodeFile(webpFilePath);
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, out);
+            out.close();
+        } catch (Exception e) {
+            Log.e("kenshin", "outputPngPath = null", e);
+            return "";
+        }
+
+        Log.d("kenshin", "outputPngPath = " + outputPngPath);
+        return outputPngPath;
+    }
 }
